f215765e0c15016aeff8159a36a156d566bd6003
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-12 15:56:12 +0300

    hooks Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types";
-import { Component } from "react";
 import css from "./Statistics.module.css";
 
-class Statistics extends Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <ul className={css.list}>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Total: {positivePercentage} %</li>
-      </ul>
-    );
-  }
+export default function Statistics({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) {
+  return (
+    <ul className={css.list}>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Total: {positivePercentage} %</li>
+    </ul>
+  );
 }
 
 Statistics.propTypes = {
@@ -24,5 +26,3 @@ Statistics.propTypes = {
   total: PropTypes.number.isRequired,
   positivePercentage: PropTypes.number.isRequired,
 };
-
-export default Statistics;
