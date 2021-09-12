21d803b8591b95e9d1f932e1c4f397cc31f42b3d
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-12 16:52:52 +0300

    hooks App v.2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const state = { good, neutral, bad };
 
   const countTotalFeedback = (feedbacks) => {
     const valuesFeedback = Object.values(feedbacks);
@@ -49,26 +50,19 @@ function App() {
     <div className="App">
       <header className="feedback">
         <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={{ good, neutral, bad }}
-            onLeaveFeedback={onLeaveFeedback}
-          />
+          <FeedbackOptions options={state} onLeaveFeedback={onLeaveFeedback} />
         </Section>
 
         <Section title="Statistics">
-          {countTotalFeedback({ good, neutral, bad }) === 0 ? (
+          {countTotalFeedback(state) === 0 ? (
             <Notification message="No feedback given" />
           ) : (
             <Statistics
               good={good}
               neutral={neutral}
               bad={bad}
-              total={countTotalFeedback({ good, neutral, bad })}
-              positivePercentage={countPositiveFeedbackPercentage({
-                good,
-                neutral,
-                bad,
-              })}
+              total={countTotalFeedback(state)}
+              positivePercentage={countPositiveFeedbackPercentage(state)}
             />
           )}
         </Section>
