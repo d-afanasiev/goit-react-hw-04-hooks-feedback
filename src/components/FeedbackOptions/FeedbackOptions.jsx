081bbc7182fe3c03d6dcd82c3f9dcd891bc51924
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-12 15:13:08 +0300

    hooks FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,22 @@
 import PropTypes from "prop-types";
-import { Component } from "react";
 import css from "./FeedbackOptions.module.css";
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    const keyObject = Object.keys(options);
-    return (
-      <div className={css.wrapperButtons}>
-        {keyObject.map((nameButton, key) => (
-          <button
-            key={key}
-            type="submit"
-            onClick={() => onLeaveFeedback(nameButton)}
-          >
-            {nameButton}
-          </button>
-        ))}
-      </div>
-    );
-  }
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const keyObject = Object.keys(options);
+  return (
+    <div className={css.wrapperButtons}>
+      {keyObject.map((nameButton, key) => (
+        <button
+          key={key}
+          type="submit"
+          onClick={() => onLeaveFeedback(nameButton)}
+        >
+          {nameButton}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.shape({
