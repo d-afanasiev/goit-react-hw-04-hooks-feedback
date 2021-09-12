7e8e7339a8ceaeea43bdd7be4b7b370fb4dd7b71
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-09-12 16:43:58 +0300

    hooks App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
 import PropTypes from "prop-types";
-import { Component } from "react";
+import { useState } from "react";
 import Statistics from "./components/Statistics";
 import FeedbackOptions from "./components/FeedbackOptions";
 import Section from "./components/Section";
 import Notification from "./components/Notification";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = (feedbacks) => {
+  const countTotalFeedback = (feedbacks) => {
     const valuesFeedback = Object.values(feedbacks);
     const totalFeedback = valuesFeedback.reduce((total, feedback) => {
       return (total += feedback);
@@ -21,7 +19,7 @@ class App extends Component {
     return totalFeedback;
   };
 
-  countPositiveFeedbackPercentage = (feedback) => {
+  const countPositiveFeedbackPercentage = (feedback) => {
     const goodFeedback = feedback.good;
     const valuesFeedback = Object.values(feedback);
     const totalFeedback = valuesFeedback.reduce((total, feedback) => {
@@ -30,42 +28,53 @@ class App extends Component {
     return Math.round((goodFeedback / totalFeedback) * 100);
   };
 
-  onLeaveFeedback = (value) => {
-    this.setState((prevState) => ({
-      [value]: prevState[value] + 1,
-    }));
+  const onLeaveFeedback = (value) => {
+    switch (value) {
+      case "good":
+        setGood((prevState) => prevState + 1);
+        break;
+      case "neutral":
+        setNeutral((prevState) => prevState + 1);
+        break;
+      case "bad":
+        setBad((prevState) => prevState + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="feedback">
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={this.state}
-              onLeaveFeedback={this.onLeaveFeedback}
-            />
-          </Section>
+  return (
+    <div className="App">
+      <header className="feedback">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={{ good, neutral, bad }}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
 
-          <Section title="Statistics">
-            {this.countTotalFeedback(this.state) === 0 ? (
-              <Notification message="No feedback given" />
-            ) : (
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotalFeedback(this.state)}
-                positivePercentage={this.countPositiveFeedbackPercentage(
-                  this.state
-                )}
-              />
-            )}
-          </Section>
-        </header>
-      </div>
-    );
-  }
+        <Section title="Statistics">
+          {countTotalFeedback({ good, neutral, bad }) === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback({ good, neutral, bad })}
+              positivePercentage={countPositiveFeedbackPercentage({
+                good,
+                neutral,
+                bad,
+              })}
+            />
+          )}
+        </Section>
+      </header>
+    </div>
+  );
 }
 
 App.propTypes = {
